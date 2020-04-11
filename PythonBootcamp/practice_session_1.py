ffaea8ddcdff94f47860e9311553cf75b6626a8c
rooslucas <rooslucas@live.nl>
2020-04-11 16:55:12 +0200

    Add first files from practice session

---
+++ PythonBootcamp/practice_session_1.py
@@ -0,0 +1,12 @@
+import keyword
+import tkinter
+
+print (keyword.kwlist)
+print ("Hello this is python")
+
+#global=1
+#File"<Interactive Input>", line 1
+#global=1
+
+#while c=10 is
+#    count=10
